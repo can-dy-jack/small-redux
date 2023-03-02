c2bd672bed09ee64f9352289179b4b578b0b89ce
can-dy-jack <jjack_chen3@163.com>
2023-03-02 11:06:30 +0800

    实现combineReducers函数

--- test/index.js
+++ test/index.js
@@ -1,7 +1,7 @@
-import createStore from '../lib/index.js';
+import { createStore, combineReducers } from '../lib/index.js';
 import asserts from 'assert';
 
-function reducer (state, action) {
+function counterReducer (state, action) {
     switch (action.type) {
         case 'IN':
             return { count: state.count + 1 };
@@ -11,19 +11,42 @@ function reducer (state, action) {
             return state;
     }
 }
+function todosReducer (state, action) {
+    switch (action.type) {
+        case 'add':
+            return { list: [...state.list, action.payload ] };
+        case 'remove':
+            return { list: state.list.slice(0, state.list.length -1 ) };
+        default:
+            return state;
+    }
+}
+const reducer = combineReducers({
+    counter: counterReducer,
+    todos: todosReducer
+});
 const { subscribe, dispatch, getState } = createStore(
-    { count: 0 },
+    { 
+        counter: {
+            count: 0 
+        },
+        todos: {
+            list: []
+        }
+    },
     reducer
 );
-let count = 0;
+let count = 0, todos = [];
 subscribe(() => {
-    count = getState().count;
-    console.log(`\x1b[32m${getState().count}\x1b[0m`)
+    let val = getState();
+    count = val.counter.count;
+    todos = val.todos.list;
+    console.log(`\x1b[32m${JSON.stringify(val, null, 4)}\x1b[0m`)
 })
 
 dispatch({ type: 'IN' });
 asserts.equal(count, 1);
-dispatch({ type: 'DE' });
-asserts.equal(count, 0);
-dispatch({ count: 8 });
-asserts.equal(count, 0);
+asserts.deepEqual(todos, []);
+dispatch({ type: 'add', payload: 'change reducer' });
+asserts.equal(count, 1);
+asserts.deepEqual(todos, ['change reducer']);
